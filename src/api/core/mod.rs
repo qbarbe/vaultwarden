ded50add449c3e62e30b9f904d34ce5c8c68f5bb
qbarbe <105909736+qbarbe@users.noreply.github.com>
2023-11-09 16:59:01 +0100

    squash
    1.30.3
    
    
    fix fmt check
    404
    
    
    update web.rs
    
    container registries
    
    fix dockerfiles
    take 3
    
    fix release
    
    1.34.3
    yet another fmt check fix
    
    Update release.yml
    Update release.yml
    
    remove unused architectures
    update gh workflows

--- src/api/core/mod.rs
+++ src/api/core/mod.rs
@@ -221,7 +221,8 @@ fn config() -> Json<Value> {
         "gitHash": option_env!("GIT_REV"),
         "server": {
           "name": "Vaultwarden",
-          "url": "https://github.com/dani-garcia/vaultwarden"
+          "url": "https://github.com/qbarbe/vaultwarden",
+          "version": crate::VERSION
         },
         "settings": {
             "disableUserRegistration": CONFIG.is_signup_disabled()
