35441e17b1e75b5e56247cb178a4e29084451b9d
qbarbe <105909736+qbarbe@users.noreply.github.com>
2024-02-07 16:54:44 +0100

    1.30.3

--- src/api/core/mod.rs
+++ src/api/core/mod.rs
@@ -222,7 +222,8 @@ fn config() -> Json<Value> {
         "gitHash": option_env!("GIT_REV"),
         "server": {
           "name": "Vaultwarden",
-          "url": "https://github.com/qbarbe/vaultwarden"
+          "url": "https://github.com/qbarbe/vaultwarden",
+          "version": crate::VERSION
         },
         "settings": {
             "disableUserRegistration": crate::CONFIG.is_signup_disabled()
